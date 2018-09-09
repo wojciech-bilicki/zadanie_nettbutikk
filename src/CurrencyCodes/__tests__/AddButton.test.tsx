4c61811662dec0a165f193f2af397cd5ac3d6c7a
Wojciech Bilicki <wojciech.bilicki@oke.com>
2018-09-09 13:00:37 +0200

    Test fix

--- src/CurrencyCodes/__tests__/AddButton.test.tsx
+++ src/CurrencyCodes/__tests__/AddButton.test.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import { render } from 'react-testing-library';
 
 import AddButton from 'src/CurrencyCodes/AddButton';
+const buttonLabel = "Add to favourites";
 
 test('calls onClick when ActionButton is pressed and display loader', () => {
   const onClickFn = jest.fn();
-  const buttonLabel = "Add to favourites";
-  const { getByText, queryByTestId, rerender } = render(<AddButton onClick={onClickFn}>{buttonLabel}</AddButton>);
-
+  const { getByText, queryByTestId, rerender } = render(<AddButton isDisabled={false} onClick={onClickFn}>{buttonLabel}</AddButton>);
   getByText(buttonLabel).click();
-
-  const loader = queryByTestId("loader");
-
   expect(onClickFn).toHaveBeenCalled();
 
 })
 
 test('should hide loader when loading is finished', () => {
   const onClickFn = jest.fn();
-  const buttonLabel = "Add to favourites";
-  const { queryByTestId } = render(<AddButton isLoading={false} onClick={onClickFn}>{buttonLabel}</AddButton>);
 
+  const { getByText, queryByTestId, rerender } = render(<AddButton isDisabled={false} onClick={onClickFn}>{buttonLabel}</AddButton>);
+  getByText(buttonLabel).click();
+
+  expect(queryByTestId('loader')).not.toBeNull();
+  rerender(<AddButton isDisabled={true} onClick={onClickFn}>{buttonLabel}</AddButton>);
   expect(queryByTestId('loader')).toBeNull();
 
 })
